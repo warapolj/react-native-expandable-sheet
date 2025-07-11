45f5c3d15161668784bfb3f6ef76ddb9e60b7408
Keng Warapol <warapolj.dev@gmail.com>
2025-07-11 18:10:22 +0700

    chore: update metro config

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,8 +1,5 @@
 const path = require('path');
 const { getDefaultConfig } = require('@expo/metro-config');
-const { getConfig } = require('react-native-builder-bob/metro-config');
-const pkg = require('../package.json');
-
 const root = path.resolve(__dirname, '..');
 
 /**
@@ -11,8 +8,15 @@ const root = path.resolve(__dirname, '..');
  *
  * @type {import('metro-config').MetroConfig}
  */
-module.exports = getConfig(getDefaultConfig(__dirname), {
-  root,
-  pkg,
-  project: __dirname,
-});
+module.exports = (async () => {
+  const { withMetroConfig } = await import('react-native-monorepo-config');
+  const defaultConfig = await getDefaultConfig(__dirname);
+
+  const config = withMetroConfig(defaultConfig, {
+    root,
+    dirname: __dirname,
+  });
+
+  config.resolver.unstable_enablePackageExports = true;
+  return config;
+})();
